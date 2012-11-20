67e266c2f3af2f29010880498d3218b30da1a3d6
Anthony Navarre <anthony.navarre@factorylabs.com>
2012-11-20 16:22:18 -0700

    Generate new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = 'c396df7df6844af3e81e2c485b6c949eebaffb051c5eeb106603325d0cfc989ccc7c8c450cc37a7203b21cb08bb467bc55538c421568091ef278fe03805ce645'
